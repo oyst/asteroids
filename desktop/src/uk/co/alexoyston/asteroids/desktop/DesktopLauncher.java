4b18131d0e6a87db31d144d7e051658c94aa4215
Alex Oyston <alexoyston93@gmail.com>
2016-10-09 17:31:51 +0100

    Initial libGDX setup

---
+++ desktop/src/uk/co/alexoyston/asteroids/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.alexoyston.asteroids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.alexoyston.asteroids.Asteroids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Asteroids(), config);
+	}
+}
---
+++ html/src/uk/co/alexoyston/asteroids/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package uk.co.alexoyston.asteroids.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import uk.co.alexoyston.asteroids.Asteroids;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Asteroids();
+        }
+}

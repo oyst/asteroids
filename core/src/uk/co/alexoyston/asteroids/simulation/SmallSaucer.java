830e43d2c5543637c29d4ad213543414286db552
Alex Oyston <alexoyston93@gmail.com>
2016-10-11 08:42:41 +0100

    Small Saucer

---
+++ core/src/uk/co/alexoyston/asteroids/simulation/SmallSaucer.java
@@ -0,0 +1,35 @@
+package uk.co.alexoyston.asteroids.simulation;
+
+public class SmallSaucer extends Entity {
+
+	public SmallSaucer() {
+		int width = 25;
+		int height = 15;
+
+		float a = width/5;
+		float b = width/3;
+		float c = height/3;
+
+		float[] vertices = new float[] {
+			width, c,
+			0, c,
+			a, 0,
+			width - a, 0,
+			width, c,
+			width - a, height - c,
+			a, height - c,
+			0, c,
+			a, height - c,
+			b, height,
+			width - b, height,
+			width - a, height - c
+		};
+
+		setVertices(vertices);
+	}
+
+	@Override
+	public void collision(Entity other) {
+	}
+
+}

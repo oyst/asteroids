489476411fc5aacece47b3c5b6932b2be9887a1b
Alex Oyston <alexoyston93@gmail.com>
2016-10-11 08:40:33 +0100

    Add Saucer

---
+++ core/src/uk/co/alexoyston/asteroids/simulation/Saucer.java
@@ -0,0 +1,34 @@
+package uk.co.alexoyston.asteroids.simulation;
+
+public class Saucer extends Entity {
+
+	public Saucer() {
+		int width = 40;
+		int height = 20;
+
+		float a = width/5;
+		float b = width/3;
+		float c = height/3;
+
+		float[] vertices = new float[] {
+			width, c,
+			0, c,
+			a, 0,
+			width - a, 0,
+			width, c,
+			width - a, height - c,
+			a, height - c,
+			0, c,
+			a, height - c,
+			b, height,
+			width - b, height,
+			width - a, height - c
+		};
+
+		setVertices(vertices);
+	}
+
+	@Override
+	public void collision(Entity other) {
+	}
+}
